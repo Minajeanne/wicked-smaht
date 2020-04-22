5f00cedcd41e1d96d494a654f8d2d9fc16aca852
Minajeanne <albrown07@mac.com>
2020-04-22 10:57:24 -0400

    added SignUp

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -25,7 +25,7 @@ export default SignIn = () => {
       <h1 className="text-3xl mb-2 text-center font-bold">Sign In</h1>
       <div className="border border-blue-400 mx-auto w-11/12 md:w-2/4 rounded py-8 px-4 md:px-8">
         {error !== null && <div className = "py-4 bg-red-600 w-full text-white text-center mb-3">{error}</div>}
-        <form className="">
+        <form className=''>
           <label htmlFor="userEmail" className="block">
             Email:
           </label>
