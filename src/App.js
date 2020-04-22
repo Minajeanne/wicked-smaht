88891a7545293aaf1defd7b799167bac131fc01d
Minajeanne <albrown07@mac.com>
2020-04-22 10:22:15 -0400

    added router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Wicked Smaht
-        </h1>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            Wicked Smaht
+          </h1>
+        </header>
+      </div>
+    </Router>
   );
 }
 
