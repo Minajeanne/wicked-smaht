9edd410490b9470b8315d54e411805952ea94ad2
Minajeanne <albrown07@mac.com>
2020-04-22 14:41:20 -0400

    implemented Tailwind >:|

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Application from "./components/Application";
 import UserProvider from "./providers/UserProvider";
-import './styles/app.css'
+import './styles/app.css';
 
 function App() {
   return (
